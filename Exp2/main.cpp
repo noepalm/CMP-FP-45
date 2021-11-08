4037786cd8da705f6f5a78e06988f730691a8646
Noemi <palmeri.1871005@studenti.uniroma1.it>
2021-11-08 11:01:36 +0100

    Edited main.cpp

--- Exp2/main.cpp
+++ Exp2/main.cpp
@@ -26,21 +26,63 @@
 using namespace std;
 
 int main(){
-    
-    TLorentzVector Meson, Pion, Kaon;
-    Meson.SetPxPyPzE(P_B,0,0,sqrt(M_B*M_B + P_B*P_B));
-    Pion.SetPxPyPzE(0,0,0, M_PION);
-    Kaon.SetPxPyPzE(0,0,0, M_K);
-    
-    TwoBodyDecay *event= new TwoBodyDecay(Meson, Pion, Kaon);
-    
-    
-    cout << "Starting invariant mass = " << (Pion+Kaon).M() << endl;
-    event->StartDecay();
-    cout << "Invariant mass of K and pi = " << (Pion+Kaon).M() << endl;
+   
+  TLorentzVector Meson, Pion, Kaon;
+  Meson.SetPxPyPzE(P_B,0,0,sqrt(M_B*M_B + P_B*P_B));
+  Pion.SetPxPyPzE(0,0,0, M_PION);
+  Kaon.SetPxPyPzE(0,0,0, M_K);
+   
+  TwoBodyDecay *event= new TwoBodyDecay(Meson, Pion, Kaon);
+   
+  event->StartDecay();
 
-    delete event;
+  cout << "Invariant mass of K and pi = " << (Pion+Kaon).M() << endl;
+   
+  // Open TFile for output
+  TString rootfname("./output.root");
+  // Overwite output file if it already exists
+  TFile rfile(rootfname, "RECREATE");
+  // Open the ROOT file and make sure the file was opened successfully.
+  // Typical cause for failure are: 1) wrong path, 2) no write privilege
+  if( !rfile.IsOpen() ) {
+   std::cout << "problems creating root file. existing... " << std::endl;
+   exit(-1);
+  }
+  std::cout << "storing output in root file " << rootfname << std::endl;
+   
+  //TH1F for truemass
+  int nbins=100;
+  int xlo=4000;
+  int xhi=7000;
+  double binwidth = (xhi-xlo) / nbins;
+  std::cout << "# bins: " << nbins << "\t bin width: " << binwidth << std::endl;
+  TH1F htruemass("truemass","distribution of the true mass",nbins,xlo,xhi);
+   
+   
+  int nsig=10000;
+   
+   
+  for(int i=0;i<nsig;++i){
+    TwoBodyDecay *eventi= new TwoBodyDecay(Meson, Pion, Kaon);
+    eventi->StartDecay();
+    double InvariantMass=(Pion+Kaon).M();
+    htruemass.Fill(InvariantMass);
+    delete eventi;
+  }
+   
+  TCanvas canvTrueMass("canvTrueMass","canvas for plotting",1280,1024);
+  htruemass.GetXaxis()->SetTitle("Distribution of the Invariant Mass [MeV]");
+  htruemass.Draw();
+  canvTrueMass.SaveAs("./true_mass.pdf");
+  canvTrueMass.SaveAs("./true_mass.jpg");
+   
+  htruemass.Write();
+   
+   
+  delete event;
 
-    return 0;
-    
+  rfile.Close();
+   
+  return 0;
+   
 }
